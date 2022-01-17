d65a024baa80cf81016acf872f977055efe78911
Ngedenidze <ngedenidze@outlook.com>
2022-01-16 20:07:27 -0500

    scale size fixed

--- javascript.js
+++ javascript.js
@@ -4,3 +4,13 @@ function myFunction() {
   element.classList.toggle("collapse");
 }
 
+function resize() {
+  var x = document.getElementById("nav_bar_resize");
+
+ 
+  if (window.screen.width <= 900) {
+    x.style.display = "block";
+  }else if (window.screen.width >= 900){
+  x.style.display = "none";
+   }
+} 
